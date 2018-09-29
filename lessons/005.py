d83821d8787fbec49e708d19d2ae4bf80abc7396
Benjamin Soyka <bensoyka@icloud.com>
2018-09-29 08:12:01 -0700

    Add lesson 005

---
+++ lessons/005.py
@@ -0,0 +1,28 @@
+# So, we've been doing a lot of addition in our examples.  We know it's very
+# simple, all you have to do is put a "+" between two numbers.  The other
+# operations are just as simple to use.
+
+x = 4
+y = 2
+
+# Here's addition:
+print(x + y)
+
+# Here's subtraction:
+print(x - y)
+
+# Here's multiplication:
+print(x * y)
+
+# Here's division:
+print(x / y)
+
+# Notice how, when run, the answer to the division problem is 2, instead of
+# just 2.  That's because 2 is an integer, and division returns a float, which
+# has a decimal.  What if we want to just have 2?
+
+print(x // y)
+
+# This code uses integer division, so it does the same thing as the last
+# division example, but with the second "/", it rounds the answer down to the
+# last whole number.
