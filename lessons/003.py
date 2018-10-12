a0c75b03a0acef7a86b7fd3f65b103c581c801dc
Benjamin Soyka <37779854+bsoyka@users.noreply.github.com>
2018-10-11 17:27:48 -0700

    Update lesson 003
    Update first and third comments.

--- lessons/003.py
+++ lessons/003.py
@@ -1,6 +1,6 @@
-# Now let's learn about saving information for later.  This is called using
+# Now, let's learn about saving information for later.  This is called using
 # variables.  Variables store information for you to use and change later.
-# Let's go back to our 1 + 2 problem again.
+# Let's go back to our 1 + 2 problem from lesson 002.
 
 x = 1
 y = 2
@@ -16,4 +16,4 @@ print(x + y)
 
 # This code makes "x" equal to 1, and "y" equal to "x" + 1.  This is the same
 # as 1 + 1, which is 2, so "y" is now equal to 2, making it the same as the
-# last bit of code.
+# last section of code.
